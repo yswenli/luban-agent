068d197ed49245d9c3287743ab6fba89ea14f490
WALLE <wenguoli_520@qq.com>
2026-08-20 14:36:16 +0800

    feat: 使用 AvaloniaEdit 替换 TextBox 显示 AI 回复内容
    - 添加 Avalonia.AvaloniaEdit 12.0.0 NuGet 包
    - 使用 TextEditor 控件支持文本选取和滚动
    - 绑定 ViewModel 的 Content 属性

--- LubanAgentCodex/Views/Controls/AssistantMessageView.axaml.cs
+++ LubanAgentCodex/Views/Controls/AssistantMessageView.axaml.cs
@@ -11,11 +11,14 @@
 *创建人：yswenli
 *电子邮箱：yswenli@outlook.com
 *创建时间：2026/8/19
-*描述：AI 助手消息显示控件
+*描述：AI 助手消息显示控件，使用 AvaloniaEdit 渲染内容
 *
 *****************************************************************************/
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaEdit;
+using LubanAgentCodex.ViewModels.Messages;
 
 namespace LubanAgentCodex.Views.Controls;
 
@@ -24,6 +27,9 @@ namespace LubanAgentCodex.Views.Controls;
 /// </summary>
 public partial class AssistantMessageView : UserControl
 {
+    private TextEditor? _editor;
+    private AssistantMessageItem? _viewModel;
+
     public AssistantMessageView()
     {
         InitializeComponent();
@@ -32,5 +38,40 @@ public partial class AssistantMessageView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+        _editor = this.FindControl<TextEditor>("Editor");
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+
+        if (DataContext is AssistantMessageItem vm)
+        {
+            _viewModel = vm;
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+            UpdateEditorContent();
+        }
+    }
+
+    private void OnViewModelPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(AssistantMessageItem.Content))
+        {
+            UpdateEditorContent();
+        }
+    }
+
+    private void UpdateEditorContent()
+    {
+        if (_editor != null && _viewModel != null)
+        {
+            var content = _viewModel.Content ?? "";
+            if (_editor.Text != content)
+            {
+                _editor.Text = content;
+                // 滚动到底部
+                _editor.ScrollToEnd();
+            }
+        }
     }
 }
