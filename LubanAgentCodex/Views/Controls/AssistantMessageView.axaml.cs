40dc0594e05308c8c75ed5c130e633797ae7c132
WALLE <wenguoli_520@qq.com>
2026-08-20 16:29:04 +0800

    revert: 恢复使用 TextBox 显示内容，移除 Markdown 渲染器

--- LubanAgentCodex/Views/Controls/AssistantMessageView.axaml.cs
+++ LubanAgentCodex/Views/Controls/AssistantMessageView.axaml.cs
@@ -11,13 +11,11 @@
 *创建人：yswenli
 *电子邮箱：yswenli@outlook.com
 *创建时间：2026/8/19
-*描述：AI 助手消息显示控件，支持 Markdown 渲染
+*描述：AI 助手消息显示控件
 *
 *****************************************************************************/
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using LubanAgentCodex.Utils;
-using LubanAgentCodex.ViewModels.Messages;
 
 namespace LubanAgentCodex.Views.Controls;
 
@@ -26,10 +24,6 @@ namespace LubanAgentCodex.Views.Controls;
 /// </summary>
 public partial class AssistantMessageView : UserControl
 {
-    private TextBlock? _contentText;
-    private AssistantMessageItem? _viewModel;
-    private string? _lastContent;
-
     public AssistantMessageView()
     {
         InitializeComponent();
@@ -38,45 +32,5 @@ public partial class AssistantMessageView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
-        _contentText = this.FindControl<TextBlock>("ContentText");
-    }
-
-    protected override void OnDataContextChanged(EventArgs e)
-    {
-        base.OnDataContextChanged(e);
-
-        if (DataContext is AssistantMessageItem vm)
-        {
-            _viewModel = vm;
-            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
-            UpdateContent();
-        }
-    }
-
-    private void OnViewModelPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == nameof(AssistantMessageItem.Content))
-        {
-            UpdateContent();
-        }
-    }
-
-    private void UpdateContent()
-    {
-        if (_contentText == null || _viewModel == null) return;
-
-        var content = _viewModel.Content ?? "";
-        
-        // 只在内容变化时更新
-        if (_lastContent != content)
-        {
-            _lastContent = content;
-            
-            // 使用 Markdown 渲染器解析内容
-            if (_contentText.Inlines != null)
-            {
-                MarkdownRenderer.Parse(content, _contentText.Inlines);
-            }
-        }
     }
 }
